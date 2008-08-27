a3d30e4e71ddc276c2efa9dad5dec24425b84759
Paul Giannaros <paul@giannaros.org>
2008-08-27 01:43:31 +0100

    Added kate.action decorator, for running a function when an action has been fired. The action decorator lets you set the a menu to place the action in, an icon, and a shortcut. Currently you cannot call kate.action at a later point and have the action dynamically added. The action adding code must be moved out of the init section to cater for that.

--- src/kate/__init__.py
+++ src/kate/__init__.py
@@ -1,16 +1,21 @@
 
 ''' Kate module. '''
 
-import pate
 import sys
+import functools
+
+import pate
 
 from PyQt4 import QtCore, QtGui
+from PyKDE4 import kdecore, kdeui
 # kate namespace
 from PyKDE4.kate import Kate
 from PyKDE4.ktexteditor import KTextEditor
 
 plugins = None
 pluginDirectories = None
+
+initialized = False
 
 # Plugin API
 
@@ -100,16 +105,54 @@ def event(**attributes):
 
 # Decorator event listeners
 
-_onInitFunctions = []
-
-@event(functions=[])
+@event(functions=set())
 def init(func):
     ''' The function will be called when Pate has loaded completely: when all
     other enabled plugins have been loaded into memory, the configuration has
     been initiated, etc. '''
-    init.functions.append(func)
+    init.functions.add(func)
     return func
 
+@event(actions=set())
+def action(text, icon=None, shortcut=None, menu=None):
+    ''' Decorator that adds an action to the menu bar. When the item is fired,
+    your function is called. Optional shortcuts, menu to place the action in,
+    and icon can be specified.
+    Parameters:
+        * text - The text associated with the action (used as the menu item
+                 label, etc).
+        * shortcut - The shortcut to fire this action or None if there is no
+                     shortcut. Must be a string such as 'Ctrl+1' or a 
+                     QKeySequence instance. By default no shortcut is set (by
+                     passing None)
+        * icon - An icon to associate with this action. It is shown alongside
+                 text in the menu bar and in toolbars as required. Pass a 
+                 string to use KDE's image loading system or a QPixmap or
+                 QIcon to use any custom icon. None (the default) sets no icon.
+        * menu - The menu under which to place this item. Must be a string 
+                 such as 'tools' or 'settings', or None to not place it in any
+                 menu. '''
+    def decorator(func):
+        a = kdeui.KAction(text, None)
+        if shortcut is not None:
+            if isinstance(shortcut, basestring):
+                a.setShortcut(QtGui.QKeySequence(shortcut))
+            else:
+                a.setShortcut(shortcut)
+        if icon is not None:
+            if isinstance(icon, basestring):
+                _icon = kdeui.KIcon(icon) # takes a string parameter
+            elif isinstance(icon, QtGui.QPixmap):
+                _icon = QtGui.QIcon(icon)
+            else:
+                _icon = icon
+            a.setIcon(_icon)
+        a.menu = menu
+        a.connect(a, QtCore.SIGNAL('triggered()'), func)
+        # delay till everything has been initialised
+        action.actions.add(a)
+        return func
+    return decorator
 
 
 # Initialisation
@@ -120,6 +163,30 @@ def pateInit():
     pluginDirectories = pate.pluginDirectories
     # wait for the configuration to be read
     def _initPhase2():
+        global initialized
+        initialized = True
+        # set up actions -- plug them into the window's action collection
+        window = application.activeMainWindow().window()
+        nameToMenu = {} # e.g "help": KMenu
+        for menu in window.findChildren(QtGui.QMenu):
+            name = str(menu.objectName())
+            if name:
+                nameToMenu[name] = menu
+        collection = window.actionCollection()
+        for a in action.actions:
+            # allow a configurable name so that built-in actions can be
+            # overriden?
+            collection.addAction(a.text(), a)
+            if a.menu is not None:
+                # '&Blah' => 'blah'
+                menuName = a.menu.lower().replace('&', '')
+                # create the menu if it doesn't exist
+                if menuName not in nameToMenu:
+                    before = nameToMenu['help'].menuAction()
+                    menu = QtGui.QMenu(a.menu)
+                    nameToMenu[menuName] = window.menuBar().insertMenu(before, menu)
+                nameToMenu[menuName].addAction(a)
+        # window.actionCollection().
         for func in init.functions:
             func()
     QtCore.QTimer.singleShot(0, _initPhase2)
@@ -128,19 +195,9 @@ pate._init = pateInit
 del pateInit
 
 
+''' The global Kate::Application instance '''
+application = Kate.application()
 
-# Testing testing 1 2 3
+''' The global document manager for this Kate application '''
+documentManager = application.documentManager()
 
-@init
-def foo():
-    while True:
-        code, success = QtGui.QInputDialog.getText(None, 'Line', 'Code:')
-        if not success:
-            break
-        success = success
-        code = str(code)
-        print '> %s' % code
-        try:
-            eval(code)
-        except SyntaxError:
-            exec code
